2e8188d1a62e28660e80980b78bfa7b1490157d1
jhay <jjhay-bot@github.com>
2021-09-28 23:58:10 +0800

    updated token

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -4,8 +4,8 @@ class Stock < ApplicationRecord
   def self.sandbox
     # require 'iex-ruby-client'
     IEX::Api::Client.new(
-      publishable_token: "Tpk_35ee6b219428402a8b6d8444d48fcf7f" ,
-      endpoint: 'https://sandbox.iexapis.com/v1'
+      publishable_token: "pk_945a6baf5076490e8e884fe0be31a140" ,
+      endpoint: 'https://cloud.iexapis.com/v1'
     )
   end
   
